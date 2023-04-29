8bc81787a3b76c571f40bb0daec5c583a6e0d092
GregariousG <91446233+GregariousG@users.noreply.github.com>
2023-04-29 12:36:46 -0700

    first draft of landing page

---
+++ frontend/src/components/Header.vue
@@ -0,0 +1,11 @@
+<script>
+    export default {
+        name: 'Landing'
+    }
+</script>
+
+
+
+<template>
+    <h1>landing PAGE header</h1>
+</template>
---
+++ frontend/src/views/LandingPage.vue
@@ -0,0 +1,16 @@
+<script>
+    import Header from "../components/Header.vue";
+    export default {
+        name: 'Landing',
+        components: {
+            Header
+        }
+    }
+</script>
+
+<template>
+    <Header />
+    <h1>You are on the landing page</h1>
+</template>
+
+
