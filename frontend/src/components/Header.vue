22ae89e8f29cef4518f5de8a495976c11238e55e
GregariousG <91446233+GregariousG@users.noreply.github.com>
2023-04-29 16:52:20 -0700

    styling and font changes to the header on landing page

--- frontend/src/components/Header.vue
+++ frontend/src/components/Header.vue
@@ -8,7 +8,10 @@
 
 <template>
     <header class = "header">
-        <div class = "AI-logo">AI|want2workout</div>
+        <div class = "AI-title1">AI|want</div>
+        <div class = "AI-title2">2</div>
+        <div class = "AI-title3">workout</div>
+        <div class = "AI-logo"></div>
     </header>
     
 </template>
