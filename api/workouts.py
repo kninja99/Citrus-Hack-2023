2f329fcc665656a1d4e710cfe96683bffe8e3398
Arnav-Menon <arnavmenon@gmail.com>
2023-04-29 23:36:19 -0700

    5 new workouts generated every time

--- api/workouts.py
+++ api/workouts.py
@@ -3,11 +3,31 @@ from flask import Flask
 
 from models import Workouts
 
+INCREMENT = 5
+
+I = 0
+J = I + INCREMENT
+SIZE = 0
+
 def gen_workout(muscle_group, experience_level, equipment):
 
     data = []
-    workouts = Workouts.query.filter(Workouts.bodyPart==muscle_group, Workouts.level==experience_level, Workouts.equipment==equipment).limit(5)
-    for w in workouts:
+    workouts = Workouts.query.filter(Workouts.bodyPart==muscle_group, Workouts.level==experience_level, Workouts.equipment==equipment).all()
+
+    global I
+    global J
+    global SIZE
+    SIZE = len(workouts)
+
+    I += INCREMENT
+    J += INCREMENT
+    if J > SIZE:
+        I = 0
+        J = I + INCREMENT
+
+    actual_workouts = workouts[I:J]
+
+    for w in actual_workouts:
         d = []
         d.append(w.title)
         d.append(w.bodyPart)
