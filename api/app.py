f95881ac491f7fef96f295f376bd5eeef243c159
Arnav-Menon <arnavmenon@gmail.com>
2023-04-29 22:18:08 -0700

    new api for workout generation added

--- api/app.py
+++ api/app.py
@@ -1,45 +1,29 @@
 import os
-from flask import Flask, jsonify
+from flask import Flask, jsonify, render_template
 from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.sql import func
 
-basedir = os.path.abspath(os.path.dirname(__file__))
-# instantiate the app
-app = Flask(__name__)
-app.config.from_object(__name__)
+from init_db import db, app
+from workouts import gen_workout
 
-# DB Setup with SQLite
-app.config['SQLALCHEMY_DATABASE_URI'] =\
-        'sqlite:///' + os.path.join(basedir, 'database.db')
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
-db = SQLAlchemy(app)
-
-
-# Create DB model
-class Workouts(db.Model):
-    id = db.Column(db.Integer, primary_key = True, autoincrement=True)
-    title = db.Column(db.String(100), nullable = False)
-    desc = db.Column(db.Text)
-    typeOfWorkout =db.Column(db.String(100))
-    bodyPart = db.Column(db.String(100))
-    equipment = db.Column(db.String(150))
-    level = db.Column(db.String(100))
-    rating = db.Column(db.Float)
-    # adding a string representation
-    def __repr__(self):
-        return f'<Workout {self.title}>'
+#TODO: remove these 3 lines when User db is up and running
+MUSCLE = "Chest"
+EXPERIENCE_LEVEL = "Intermediate"
+# EQUIPMENT = ["Dumbbell"]
+EQUIPMENT = "Dumbbell"
 
 # enable CORS
 CORS(app, resources={r'/*': {'origins': '*'}})
-
 
 # sanity check route
 @app.route('/ping', methods=['GET'])
 def ping_pong():
     return jsonify('pong!')
 
+@app.route('/generate', methods=['GET'])
+def generate_workout():
+    workouts = gen_workout(MUSCLE, EXPERIENCE_LEVEL, EQUIPMENT)
 
 if __name__ == '__main__':
     app.run()
