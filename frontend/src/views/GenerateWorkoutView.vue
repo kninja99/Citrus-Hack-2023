47a5378c6d13d1fe6b859cccf0847d780d19f663
Kninja99 <kcdamschen@yahoo.com>
2023-04-30 00:11:05 -0700

    loading a scuffed table

--- frontend/src/views/GenerateWorkoutView.vue
+++ frontend/src/views/GenerateWorkoutView.vue
@@ -1,9 +1,25 @@
 <script>
+import axios from 'axios';
 import Header from "../components/Header.vue";
 export default {
-    name: 'Landing',
+    data: function () {
+        return {
+            workout: null
+        };
+    },
     components: {
         Header
+    },
+    methods: {
+        generateWorkout: async function () {
+            await axios.get('http://localhost:5000/generate')
+                .then((res) => {
+                    this.workout = res.data;
+                })
+                .catch((err) => {
+                    console.log(err);
+                })
+        }
     }
 }
 </script>
@@ -12,7 +28,19 @@ export default {
     <div class="container">
         <Header />
         <div class="generate-workout">
-            <div class="workout"></div>
+            <div class="workout"><button @click="this.generateWorkout" class="form-btn">Generate Workout</button></div>
+            <table v-if="this.workout" class="workout-table">
+                <tr>
+                    <th>Workout Name</th>
+                    <th>Muscle Group</th>
+                    <th>Difficulty</th>
+                    <th>sets</th>
+                    <th>reps</th>
+                </tr>
+                <tr v-for="workout in this.workout">
+                    <td v-for="data in workout">{{ data }}</td>
+                </tr>
+            </table>
         </div>
     </div>
 </template>
